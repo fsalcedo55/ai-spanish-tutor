ae41bc88fa12ac71bd584b86501c36d480ff5b6b
fsalcedo55 <fsalcedo55@hotmail.com>
2024-10-25 21:15:39 -0400

    update height in mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ChatInterface from "../components/ChatInterface"
 export default function Home() {
   return (
     <div
-      className="flex flex-col items-center justify-start sm:justify-center min-h-screen bg-cover bg-center bg-no-repeat"
+      className="flex flex-col items-center justify-start sm:justify-center h-[calc(90vh)] sm:min-h-screen bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: "url('/grass-city.svg')",
         backgroundSize: "cover",
